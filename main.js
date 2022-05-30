2ace2238e2d7f94c8b73892b2b5b7126998c2a41
NoobMaester <joshstilez@gmail.com>
2022-05-30 11:48:40 +0200

    Logo

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const navToggle = document.querySelector('.hamburger');
 
+const navLink = document.querySelectorAll('.nav_link');
+
 navToggle.addEventListener('click', ()=> {
     document.body.classList.toggle('nav_open');
+});
+
+navLink.forEach(link => {
+    link.addEventListener('click' , () => {
+        document.body.classList.remove('nav_open');
+    });
 });
